59c3931d6d9514878c0c81bd8f0a49c3f6b3f03f
Coolis1362 <tadeotherocketbuilder@outlook.com>
2025-05-03 07:40:54 -0400

    Update KERNEL_VERSION in ts-distro-distro_name.py to match TS-KERNEL RHT 1.0.0

--- rpmbuild/SOURCES/boot/ts-distro-distro_name.py
+++ rpmbuild/SOURCES/boot/ts-distro-distro_name.py
@@ -61,7 +61,7 @@ def put_distro_name_here(): # Replace put_distro_name_here with the name of your
     MAIN_USER_NAME = "ADMIN_USER"
     DISTRO_NAME_PREFIX = "TS-DISTRO"
     DISTRO_NAME = "distro_name" # Replace distro_name with the name of your distro
-    KERNEL_VERSION = "1.0.1.1 Codename 'Pulse'" 
+    KERNEL_VERSION = "TS-KERNEL RHT 1.0.0 Codename 'Pulse'" 
     DISTRO_VERSION = "version name" # Replace This with Version name Of your Distor e.g TS-DISTRO MAIN >>1.0.1pa2<< (The >> and << Are Pointing to A Version Number As A Version number)
     GITHUB_RESPOS_URL = "https://github.com/Coolis1362/"
     TSCLI_VERSION = "1.0.1.1"
